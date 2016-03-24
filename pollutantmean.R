d71b8b4fe5875238c002a3bd373e543fea3dab7f
katharine-atx <rovinsky.katharine@gmail.com>
2016-03-23 22:18:42 -0500

    Updated pollutant mean function

--- pollutantmean.R
+++ pollutantmean.R
@@ -1,11 +1,11 @@
 #Part 1: Pollutant monitoring data...
 
-#polutantmean function to calculate mean sulfate or nitrate across a defined monitor list
+#pollutantmean function to calculate mean sulfate or nitrate across a defined monitor list
 	#directory (character): local location of monitor data
 	#pollutant (character): column name: "sulfate" or "nitrate"
 	#id (integer): monitor ID #
 
-polutantmean <- function(directory, pollutant, id = 1:332, removeNA = TRUE) {
+pollutantmean <- function(directory, pollutant, id = 1:332, removeNA = TRUE) {
 	val <- numeric(mean(pollutant, na.rm = removeNA))
 	val
 }
