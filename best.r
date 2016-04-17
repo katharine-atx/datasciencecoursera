6492fe1bbc1d0e1998006228a759ea61fa27a1a3
katharine-atx <rovinsky.katharine@gmail.com>
2016-04-17 18:44:33 -0500

    Updated best.R function to identify best hospital in state for given outcome

--- best.r
+++ best.r
@@ -1,12 +1,11 @@
 # R Programming: Assignment 3 - Federal Hospital Stats and Patient Outcome Rankings
-# Part 2: Function to fund best hospital in a state
+# Part 2: Function to find best hospital in a state
 
 #best function takes two arguments: 2-character state abbreviation and an
 #outcome name and returns a character vector with the name of the hospital that 
 #has the lowest 30-day mortality for the specifed outcome in that state.
   
   #Data file: "outcome-of-care-measures.csv"
-	
 	#Input arguments:
 	   #State (character), i.e. "AL"
 	   #outcome (character), one of "heart attack", "heart failure", or "pneumonia"
@@ -14,11 +13,9 @@
 		#Hospital.Name (character)
 
 #Note: Hospitals that do not have data on a particular outcome will be excluded.
-
-#Note: If an invalid state value is passed to best, the function should throw an 
+#If an invalid state value is passed to best, the function should throw an 
 #error via the stop function with the message "invalid state". 
-
-#Note: If an invalid outcome value is passed to best, the function should throw 
+#If an invalid outcome value is passed to best, the function should throw 
 #an error via the stop function with the message "invalid outcome".
 
 #Tie-breaker: In case of tie for the best hospital for a given outcome,the 
@@ -28,43 +25,46 @@
 
 best <- function(state, outcome) {
 #Reading in the data...
-outcome <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
+data <- read.csv("outcome-of-care-measures.csv", colClasses = "character")
 
 #Checking that state and outcome inputs are valid...
-if as.character(state) !%in% unique(outcome$state){
-	message("invalid state") ### how to stop the function????
-	#stop
+if ((state %in% unique(data$State)) == FALSE){
+	stop("invalid state") 
 	}
-if as.character(outcome) !%in% c("heart attack", "heart failure", "pneumonia"){
-	message("invalid outcome")
-	#stop
+if ((outcome %in% c("heart attack", "heart failure", "pneumonia")) 
+	== FALSE){
+	stop("invalid outcome")
 	}
 #If valid, proceed to subset data for chosen state...
-state_subset <- outcome[which(outcome$State == state),]
+state_subset <- data[which(data$State == state),]
 
-#How to map the input values to the columns???
+#Column 11 contains heart attack mortality rate:
+if (outcome %in% {"heart attack"}){
+	colnames(state_subset)[11] <- "outcome"
+	}
+#Column 17 contains heart failure mortality rate:
+if (outcome %in% {"heart failure"}){
+	colnames(state_subset)[17] <- "outcome"
+	}
+#Column 23 contains pneumonia mortality rate:
+if (outcome %in% {"pneumonia"}){
+	colnames(state_subset)[23] <- "outcome"
+	}
 
-# column 11 contains heart attack mortality rate:
-"heart attack" <- as.numeric(outcome[, 11]) 
-# column 17 contains heart failure mortality rate:
-"heart failure" <- as.numeric(outcome[, 17]) 
-# column 23 contains pneumonia mortality rate:
-"pneumonia" <- as.numeric(outcome[, 23]) 
+## Return hospital name in that state with lowest 30-day death
+## rate for the outcome
+min_rate <- min(as.numeric(state_subset$outcome[!is.na(as.numeric(state_subset$outcome))]))
+hospital <- state_subset$Hospital.Name[which(as.numeric(state_subset$outcome) == min_rate)]
 
-hospital <- state_subset[which(outcome_chosen == min(outcome_chosen), Hospital.Name]
-hospital_v <- hospital ######
-
-if length(hospital) > 1{
-	sorted <- sort(hospital) #
-	return sorted[1]
-else
-	return hospital}
+if (length(hospital) > 1){
+	sorted <- sort(hospital) 
+	return(sorted[1])
+	stop()
+	}
+return(hospital)
  }
 
 
-
-## Return hospital name in that state with lowest 30-day death
-## rate
 
 
 
---
+++ rankall.R
@@ -0,0 +1,2 @@
+# R Programming: Assignment 3 - Federal Hospital Stats and Patient Outcome Rankings
+# Part 4: Return hospital name given outcome metric and national rank
